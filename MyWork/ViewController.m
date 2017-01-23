b1667356d09fcad67f9829345e739f2f735e226a
赵大成 <yidong-ios7@yhd.com>
2017-01-23 11:23:24 +0800

    Initial Commit

---
+++ MyWork/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyWork
+//
+//  Created by 赵大成 on 2017/1/23.
+//  Copyright © 2017年 赵大成. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
